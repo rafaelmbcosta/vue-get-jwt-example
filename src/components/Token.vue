4a9fa2aa2b5a3b7155de972130dd8495f49ab5d2
Rafael Costa <rafael.costa@pge.ce.gov.br>
2018-09-21 11:15:39 -0300

    Getting JWT token

---
+++ src/components/Token.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div>
+    <h2> Testando Token </h2>
+    <p v-if="token"> {{ token }} </p>
+    <button @click="setToken"> Setar Token</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function(){
+    return{
+      token: null
+    }
+  },
+  methods: {
+    setToken(){
+      axios.post('http://localhost:3000/user_token', {
+        auth: {
+          email: 'nome@email.com',
+          password: '123456'
+        }
+      })
+      .then(response => {
+        this.token = response.data.jwt
+      })
+      .catch(error => {
+        this.token = 'Erro ao recuperar token'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
